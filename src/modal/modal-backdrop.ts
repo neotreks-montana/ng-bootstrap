5e04fe10ffe49130fd387aea3dce77fabe27c041
Tanner Grier <montana@neotreks.com>
2021-04-01 12:33:21 -0600

    Run clang-format on src/ directory, fixing formatting for modal-backdrop.

--- src/modal/modal-backdrop.ts
+++ src/modal/modal-backdrop.ts
@@ -4,8 +4,7 @@ import {Component, Input, ViewEncapsulation} from '@angular/core';
   selector: 'ngb-modal-backdrop',
   encapsulation: ViewEncapsulation.None,
   template: '',
-  host:
-      {'[class]': '"modal-backdrop fade show" + (backdropClass ? " " + backdropClass : "")'}
+  host: {'[class]': '"modal-backdrop fade show" + (backdropClass ? " " + backdropClass : "")'}
 })
 export class NgbModalBackdrop {
   @Input() backdropClass: string;
